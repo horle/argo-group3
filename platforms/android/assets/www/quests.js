72c5ac73c082776f1d25348203354ff3d81bd952
horle <felix.kussmaul@student.uni-siegen.de>
2016-01-08 01:21:51 +0100

    changed project structure, added cordova

---
+++ platforms/android/assets/www/quests.js
@@ -0,0 +1,20 @@
+var questions = {
+	"questions": [
+		{
+			"title": "Fragentitel",
+			"text": "Wie hie&szlig; der erste r&ouml;mische Kaiser?",
+			"answers": [
+				{
+					"text": "Nero",
+					"correct": false
+				},{
+					"text": "Augustus",
+					"correct": true
+				},{
+					"text": "Octavian",
+					"correct": false
+				}
+			]
+		}
+	]
+};
---
+++ www/js/functions.js
@@ -0,0 +1,69 @@
+var failCount;
+
+function popQuestion(id) {
+	
+	//get question by id
+	var q = questions.questions[id];
+
+	//counter for wrong answers. one less than number of possibilities
+	failCount = q.answers.length - 1;
+	
+	var rad = "";
+	//create radio buttons for answers	
+	for (var i = 0; i < q.answers.length; i++) {
+		
+		rad += "<div class=\"answer\"><input class=\"visibleInput\" type=\"radio\" name=\"question\" value="
+			+ q.answers[i].correct + "><span>" + q.answers[i].text + "</span></div><br>";
+	}
+	
+	var that = this;
+	swal({
+		title: q.title,
+		text: q.text + "<br>"+rad,
+		html: true,
+		closeOnConfirm: false
+	}, function() {
+			ret = that.check();
+			if (ret === false){
+				if (failCount === 1)
+					swal.showInputError("Leider falsch! Noch 1 Versuch übrig.");
+				else
+					swal.showInputError("Leider falsch! Noch "+failCount+" Versuche übrig.");
+					
+				return false;
+			} else
+			if (ret === null)
+				swal.showInputError("Bitte eine Antwort auswählen!");
+		}
+	);
+}
+
+function check(){ 
+	//get checked radio button value
+	var res = document.querySelector('input[name="question"]:checked')
+	if (res === null){
+		return null;
+	}
+	else
+		res = res.value;
+	
+	if (res == "true"){
+//		score += failCount * 10;
+		swal("Richtig!", "Gut gemacht. Auf zu neuen Aufgaben ...", "success");
+
+	} else {
+
+		failCount--;
+		if (failCount == 0){
+			swal("Vergeigt!", "Für diese Aufgabe gibt es leider keine Punkte.", "error");
+		}
+		return false;
+	}
+}
+
+// circle js code
+$('xp-circle').circleProgress({
+    value: 0.6
+}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(parseInt(100 * progress) + '<i>%</i>');
+});
